9fcf9f7d6a115429efe0501c7b024ec996648d3e
shakthi01912 <it18211924@my.sliit.lk>
2024-09-18 22:32:00 +0100

    Intial commit

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react"
+import { NewTodoForm } from "./TaskDashboard/NewTodoForm"
+import "./TaskDashboard/styles.css"
+import { TodoList } from "./TaskDashboard/TodoList"
+
+export default function App() {
+
+<h1>welcome</h1>
+
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
+
+    return JSON.parse(localValue)
+  })
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
+
+  function addTodo(title) {
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title, completed: false },
+      ]
+    })
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
+  return (
+    <>
+      <NewTodoForm onSubmit={addTodo} />
+      <h1 className="header">Todo List</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+    </>
+  )
+}
